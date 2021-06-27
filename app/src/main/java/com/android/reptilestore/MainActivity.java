e119ad21f8a0b1de9b7cf9225a30456aabcb58b4
Maksuda19 <maksudaakhi@gmail.com>
2021-06-28 02:01:34 +0900

    Revert "Added Settings Page"
    This reverts commit 48a2bc64ad8ebe0d84e7c6d54422a39cecc8834d.

--- app/src/main/java/com/android/reptilestore/MainActivity.java
+++ app/src/main/java/com/android/reptilestore/MainActivity.java
@@ -1,10 +1,6 @@
 package com.android.reptilestore;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.example.reptilestore.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -37,25 +33,6 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
-
     }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.top_menu, menu);
-        return true;
-    }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.navigation_settings) {
-            Intent intent = new Intent();
-            intent.setClassName(this, "com.android.reptilestore.SettingsActivity");
-            startActivity(intent);
-            return true;
-        }
-        return false;
-    }
-
 
 }
