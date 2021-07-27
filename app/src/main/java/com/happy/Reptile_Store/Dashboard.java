9586fcf13f0e523bd79133d8d85abb2b3e652bc3
Fathyma12-git <84936000+Fathyma12-git@users.noreply.github.com>
2021-07-27 00:36:16 -0400

    Dashboard
    Dashboard design

--- app/src/main/java/com/happy/Reptile_Store/Dashboard.java
+++ app/src/main/java/com/happy/Reptile_Store/Dashboard.java
@@ -1,12 +1,16 @@
 package com.happy.Reptile_Store;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.card.MaterialCardView;
+import com.google.firebase.auth.FirebaseAuth;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class Dashboard extends AppCompatActivity {
-    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +18,19 @@ public class Dashboard extends AppCompatActivity {
         setContentView(R.layout.activity_dashboard);
 
 
+
+    }
+
+    public void logout(View view){
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.signOut();
+        Intent intent =new Intent(Dashboard.this,Login.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void contact(View view){
+        Intent intent =new Intent(Dashboard.this,contact_us.class);
+        startActivity(intent);
     }
 }
